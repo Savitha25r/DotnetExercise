9b457541850aee17def864e2f5b8d292dd47cfad
savitha25r <sarithanair25r@gmail.com>
2022-09-28 12:07:00 -0700

    Assignments

---
+++ program7c/program7c/Program.cs
@@ -0,0 +1,78 @@
+﻿
+    using System;
+public class program7c
+{
+    public static void Main()
+    {
+
+        int mx;
+        int mn;
+
+
+
+        int[] marks = new int[10];
+
+
+
+
+
+        //Inputs -- Marks
+
+
+
+        for (int i = 0; i < marks.Length; i++)
+        {
+            marks[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+        mx = marks[0];
+        mn = marks[0];
+
+
+
+        //finding min and max marks
+        for (int j = 1; j < marks.Length; j++)
+        {
+            if (marks[j] < mn)
+            {
+                mn = marks[j];
+            }
+
+
+
+            if (marks[j] > mx)
+            {
+                mx = marks[j];
+            }
+        }
+
+
+
+
+        //printing  max marks
+        Console.WriteLine("maximum mark is " + mx);
+       
+        Console.ReadLine();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ program7d/programcd/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programcd
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int mx;
+            int mn;
+
+
+
+            int[] marks = new int[10];
+
+
+
+
+
+            //Inputs -- Marks
+
+
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+            mx = marks[0];
+            mn = marks[0];
+
+
+
+            //finding min and max marks
+            for (int j = 1; j < marks.Length; j++)
+            {
+                if (marks[j] < mn)
+                {
+                    mn = marks[j];
+                }
+
+
+
+                if (marks[j] > mx)
+                {
+                    mx = marks[j];
+                }
+            }
+
+
+
+
+            //printing  min marks
+           
+            Console.WriteLine("minimum mark is " + mn);
+            Console.ReadLine();
+
+        }
+    }
+}
